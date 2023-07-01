d9694136e5d41301d9055cbe885fa6a0cfbdb4a1
Darshan Kanani <darshan.kanani.05@gmail.com>
2023-07-01 18:57:50 +0530

    Navbar

---
+++ partials/_navbar.php
@@ -0,0 +1,14 @@
+<div class="logo">
+    <a href="main.html"><img src="https://i.ibb.co/kDVwgwp/logo.png" alt="RedStore" width="125px" /></a>
+</div>
+<nav>
+    <ul id="MenuItems">
+        <li><a href="main.html">Home</a></li>
+        <li><a href="product.html">Products</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="account.html">Account</a></li>
+    </ul>
+</nav>
+<a href="cart.html"><img src="https://i.ibb.co/PNjjx3y/cart.png" alt="" width="30px" height="30px" /></a>
+<img src="https://i.ibb.co/6XbqwjD/menu.png" alt="" class="menu-icon" onclick="menutoggle()" />
